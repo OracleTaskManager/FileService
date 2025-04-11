c74e7b90c425eeec4eb58b3bbaad03855a7e40d3
Iñaki GM <inakiglzm@outlook.com>
2025-04-11 14:54:05 -0600

    Added Swagger Documentation

--- src/main/java/com/Oracle/FileService/FileServiceApplication.java
+++ src/main/java/com/Oracle/FileService/FileServiceApplication.java
@@ -1,12 +1,17 @@
 package com.Oracle.FileService;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.Oracle.FileService")
 public class FileServiceApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing() // Optional: avoids crash if .env is missing
+				.load();
+
+		System.setProperty("JWT_SECRET_ORACLE", dotenv.get("JWT_SECRET_ORACLE"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(FileServiceApplication.class, args);
 	}
 
--- src/test/java/com/Oracle/FileService/FileServiceApplicationTests.java
+++ src/test/java/com/Oracle/FileService/FileServiceApplicationTests.java
@@ -1,5 +1,4 @@
 package com.Oracle.FileService;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
